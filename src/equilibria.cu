e48b87ab1e1a1b03950dc1eeb4701d85d763fd74
Horatio Caine <hxcaine@gmail.com>
2013-02-28 13:51:27 +0000

    Added Michal's init functions, a run script and a makefile
    - Added Michal's init functions from the equilibria.c file into the .cu file.
    - Modified the appropriate code to make it work for NVCC (e.g. casting mallocs into the required type)
    - Added a makefile for equilibria.cu
    - Added a small run script which runs make and then the script with 16 blocks each containing 16 threads (chosen at random, should be changed when necessary)

--- src/equilibria.cu
+++ src/equilibria.cu
@@ -1,37 +1,57 @@
 #include "cutil_inline.h"
 
-//#define BLOCK_SIZE 32
-//#define GRID_SIZE 32
+#include "math.h"
+#include "time.h"
+#include "stdlib.h"
+#include "limits.h"
+#include "stdio.h"
 
 #define ITERS 10
 
+//#define BLOCK_SIZE 32
+//#define GRID_SIZE 32
+#define NUM_MANUFACTURERS 2
+#define NUM_CONSUMERS 10
+#define MAX_MARGINAL 250
+#define BASE_INCOME 20000
+
+const char* products[] = {"milk", "bread", "toilet_paper", "butter", "jam", "cheese"};
+
+int select_loyalty();
+double gaussrand();
+double positive_gaussrand();
+
 typedef struct
 {
-  char* name
+  char* name;
   int product_num;
   float price;
   float m_cost;
 } product;
 
+
+
 typedef struct
 {
-    int loyalty;
-    float income;
-    float bank_balance;
-    int* product_req;
+  int loyalty;
+  float income;
+  float bank_balance;
+  int* product_req;
 } consumer;
 
 typedef struct
 {
-    int num;
-    float bank_balance;
-    product** products;
+  int num;
+  float bank_balance;
+  product** products;
 } manufacturer;
 
 // First dimension is product ID
 // Second dimension is manufacturer ID
 // Price is in pence
 int** price; // 2D array
+
+int* marginal_cost;
 
 // First dimension is product ID
 // Second dimension is consumer ID
@@ -53,19 +73,7 @@ typedef struct
   int* today;
 } profits;
 
-
-// Rand*MC*3 (roughly)
-void init_prices() { // Mr Michalewicz
-}
-
-// Uniformly distributed
-void init_loyalty() { // Mr Michalewicz
-}
-
-// Gaussian over population
-void init_income() { // Mr Michalewicz
-}
-
+/*
 // Get the manufacturer ID fom which the consumer chooses to 
 // purchase the given product
 __device__ int consumer_choice(int consumer_id, int product_id) {
@@ -81,14 +89,167 @@ __global__ void equilibriate(int** price, int** consumption, int* income, int* l
 
 // Get the manufacturer ID fom which the consumer chooses to 
 // purchase the given product
+int host_consumer_choice(int consumer_id, int product_id) {
+  
+}
+
+// Get tomorrow's price for the given product ID
+int host_price_response(int manufacturer_id, int product_id) {
+}
+
+void host_equilibriate(int** price, int** consumption, int* income, int* loyalty, profits* profit) {
+}
+*/
+
+void init_marginal()
+{
+  marginal_cost = (int*) malloc((sizeof(products)/sizeof(char*)) * sizeof(int));
+
+  int i;
+    
+  for (i = 0; i < sizeof(products)/sizeof(char*); ++i) {
+    float rval = (float)rand()/RAND_MAX;
+    marginal_cost[i] = (int)(rval * MAX_MARGINAL);
+    printf("Marginal cost for %s is %d.\n", products[i], marginal_cost[i]);
+  }
+}
+
+// Rand*MC*3 (roughly)
+void init_prices()
+{
+  int i;
+  int j;
+
+  price = (int**) malloc(sizeof(products)/sizeof(char*) * sizeof(int*));
+
+  for (i = 0; i < sizeof(products)/sizeof(char*); ++i) {
+    price[i] = (int*) malloc(NUM_MANUFACTURERS * sizeof(int));
+    for (j = 0; j < NUM_MANUFACTURERS; ++j) {
+      float rval = (float)rand()/RAND_MAX;
+      price[i][j] = marginal_cost[i] + (rval * marginal_cost[i]);
+      printf("Price for %s from manufacturer %d: %d\n", products[i], j, price[i][j]);
+    }
+  }
+}
+
+// Uniformly distributed
+void init_loyalty()
+{
+  loyalty = (int*) malloc(NUM_CONSUMERS * sizeof(int));
+    
+  int i;
+  int* counts = (int*) malloc(NUM_MANUFACTURERS * sizeof(int));
+    
+  for (i = 0; i < NUM_CONSUMERS; ++i) {
+    loyalty[i] = select_loyalty();
+    //	printf("Customer %d loyal to manufacturer %d\n", i, loyalty[i]);
+    counts[loyalty[i]]++;
+  }
+
+  for (i = 0; i < NUM_MANUFACTURERS; ++i) {
+    printf("Manufacturer %d has %d loyal customers.\n", i, counts[i]);
+  }
+}
+
+int select_loyalty()
+{
+  int i;
+  float rval = (float)rand()/RAND_MAX;
+
+  float split = 1.0/NUM_MANUFACTURERS;
+  for (i = 0; i < NUM_MANUFACTURERS; ++i) {
+    if (rval < split * (i + 1))
+      return i;
+  }
+  return i;
+}
+
+/*
+ * Gaussian over population. Currently generates values using a gaussian tail
+ * distribution - there will be a lot of people who have an income around 
+ * the base income, and fewer with higher incomes.
+ */
+void init_income()
+{
+  income = (int*) malloc(NUM_CONSUMERS * sizeof(int));
+    
+  int i;
+  for (i = 0; i < NUM_CONSUMERS; ++i) {
+    income[i] = BASE_INCOME * (positive_gaussrand() + 1);
+    printf("Income of household %d: %d\n", i, income[i]);
+  }
+}
+
+/*
+ * Generate a gaussian random value in the interval [0,infinity]
+ */
+double positive_gaussrand()
+{
+  double r;
+  while ((r = gaussrand()) < 0);
+  return r;
+}
+
+// Polar method implementation taken from c-faq.com/lib/gaussian.html
+double gaussrand()
+{
+  static double V1, V2, S;
+  static int phase = 0;
+  double X;
+    
+  if (phase == 0){
+    do {
+      double U1 = (double)rand()/RAND_MAX;
+      double U2 = (double)rand()/RAND_MAX;
+	    
+      V1 = 2 * U1 - 1;
+      V2 = 2 * U2 - 1;
+      S = V1 * V1 + V2 * V2;
+    } while (S >= 1 || S == 0);
+    X = V1 * sqrt(-2 * log(S) / S);
+  } else {
+    X = V2 * sqrt(-2 * log(S) / S);
+  }
+
+  phase = 1 - phase;
+    
+  return X;
+}
+
+
+
+// Get the manufacturer ID fom which the consumer chooses to 
+// purchase the given product
 int host_consumer_choice(int consumer_id, int product_id) { // Mr Caine
+    return 0;
 }
 
 // Get tomorrow's price for the given product ID
 int host_price_response(int manufacturer_id, int product_id) { // Mr Caine
+    return 0;
 }
 
 void host_equilibriate(int** price, int** consumption, int* income, int* loyalty, profits* profit) {
+}
+
+void copy_array(float* from, float* to, unsigned int size) {
+  for (int i = 0; i < size; i++) {
+    to[i] = from[i];
+  }
+}
+
+void print_array(float* data_in, unsigned int size) {
+  for (int i=0; i < size; i++) {
+    printf("%f,", data_in[i]);
+  }
+  printf("\n\n");
+}
+
+double sum_array(float* data_in, unsigned int length) {
+  double rezult = 0;
+  for (int k=0; k < length; k++)
+    rezult += data_in[k];
+  return rezult;
 }
 
 
@@ -113,26 +274,6 @@ void host_equilibriate(int** price, int** consumption, int* income, int* loyalty
   }
   }  */
 
-void copy_array(float* from, float* to, unsigned int size) {
-  for (int i = 0; i < size; i++) {
-    to[i] = from[i];
-  }
-}
-
-void print_array(float* data_in, unsigned int size) {
-  for (int i=0; i < size; i++) {
-    printf("%f,", data_in[i]);
-  }
-  printf("\n\n");
-}
-
-double sum_array(float* data_in, unsigned int length) {
-  double rezult = 0;
-  for (int k=0; k < length; k++)
-    rezult += data_in[k];
-  return rezult;
-}
-
 
 
 /* First arg: threads per block,
@@ -154,7 +295,20 @@ int main(int argc, char** argv)
   cutilSafeCall(cudaGetDevice(&devID));
   cutilSafeCall(cudaGetDeviceProperties(&props, devID));
 
+  // ---------------------------------------------
 
+  srand(time(NULL));
+
+  /* int i; */
+  /* for (i = 0; i < 100; ++i) { */
+  /*   printf("%lf\n", positive_gaussrand() + 1); */
+  /* } */
+
+
+  init_income();
+  init_loyalty();
+  init_marginal();
+  init_prices();
 
   
 
